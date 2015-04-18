406d7a73644dc07e3878961f8ebb04f67722994a
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-18 18:24:02 +0200

    Started again new class added

---
+++ RamonGilabert/RamonGilabert/RGMainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RGMainViewController.swift
+//  
+//
+//  Created by Ramon Gilabert Llop on 4/18/15.
+//
+//
+
+import UIKit
+
+class RGMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

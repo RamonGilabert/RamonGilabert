ddc43eee0b188bfa9cd64a5eb1b5de5056768a1d
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-18 19:20:26 +0200

    Added different view controllers

---
+++ RamonGilabert/RamonGilabert/RGProjectsViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class RGProjectsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ RamonGilabert/RamonGilabert/RGSkillsViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class RGSkillsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}

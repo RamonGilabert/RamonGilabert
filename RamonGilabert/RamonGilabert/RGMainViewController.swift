742de73210fc2ba685605400a59f05617c174187
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-18 19:09:28 +0200

    Instantiated array with nil objects

--- RamonGilabert/RamonGilabert/RGMainViewController.swift
+++ RamonGilabert/RamonGilabert/RGMainViewController.swift
@@ -11,7 +11,7 @@ class RGMainViewController: UIViewController {
     let deviceWidth = UIScreen.mainScreen().bounds.width
     let deviceHeight = UIScreen.mainScreen().bounds.height
     var scrollView = UIScrollView()
-    var arrayWithControllers = []
+    var arrayWithControllers = NSMutableArray()
 
     // MARK: View lifecycle
 
@@ -23,6 +23,12 @@ class RGMainViewController: UIViewController {
         self.scrollView.backgroundColor = UIColor.whiteColor()
         self.scrollView.pagingEnabled = true
         self.view.addSubview(self.scrollView)
+
+        for index in 0...2 {
+            self.arrayWithControllers.addObject(NSNull())
+        }
+
+        loadScrollViewInPage(0)
     }
 
     // MARK: ScrollView methods
@@ -34,42 +40,14 @@ class RGMainViewController: UIViewController {
 
         let viewController = self.arrayWithControllers[page] as? UIViewController
 
+        if !(viewController != nil) {
+            if page == 0 {
+
+            } else if page == 1 {
+
+            } else {
+
+            }
+        }
     }
 }
-
-//- (void)loadScrollViewWithPage:(NSUInteger)page
-//{
-//    if (page >= self.numberOfPages) return;
-//
-//    UIView *view = [self.arrayWithSlides objectAtIndex:page];
-//
-//    if ((NSNull *)view == [NSNull null]) {
-//        view = [UIView new];
-//        [self.arrayWithSlides replaceObjectAtIndex:page withObject:view];
-//    }
-//
-//    if (view.superview == nil)
-//    {
-//        CGRect frame = self.frame;
-//        frame.origin.x = CGRectGetWidth(frame) * page;
-//        frame.origin.y = 0;
-//        view.frame = frame;
-//
-//        [self addSubview:view];
-//    }
-//
-//    if ((int)(page + 1) == (int)self.arrayWithSlides.count) {
-//        if (!self.buttonDismiss) {
-//            self.buttonDismiss = [[UIButton alloc] initWithFrame:CGRectMake(50, self.deviceHeight - self.deviceHeight/5.5, self.deviceWidth - 100, 65)];
-//            self.buttonDismiss.layer.cornerRadius = 7.5;
-//            self.buttonDismiss.layer.borderColor = [UIColor darkGrayColor].CGColor;
-//            self.buttonDismiss.layer.borderWidth = 2;
-//            self.buttonDismiss.titleLabel.font = [UIFont fontWithName:@"Avenir" size:26];
-//            [self.buttonDismiss setTitle:@"Start this journey" forState:UIControlStateNormal];
-//            [self.buttonDismiss setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
-//            [self.buttonDismiss addTarget:self action:@selector(onDismissButtonPressed) forControlEvents:UIControlEventTouchUpInside];
-//            self.buttonDismiss.alpha = 0;
-//            [view addSubview:self.buttonDismiss];
-//        }
-//    }
-//}

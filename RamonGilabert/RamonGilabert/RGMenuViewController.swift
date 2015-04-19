10d624d2dbce2ad3ff56e057ae3c611943cba090
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-19 13:46:44 +0200

    Even more refactor

--- RamonGilabert/RamonGilabert/RGMenuViewController.swift
+++ RamonGilabert/RamonGilabert/RGMenuViewController.swift
@@ -19,12 +19,52 @@ class RGMenuViewController: UIViewController {
         blurrView.frame = CGRectMake(0, 0, Constant.Size.DeviceWidth, Constant.Size.DeviceHeight)
         self.view.addSubview(blurrView)
 
-        self.storyButton = self.viewModel.buttonInMenu(Constant.Positioning.XPositionLeftMenuButton, yPosition: Constant.Size.DeviceHeight / 7.5, image: "story-image", text: "STORY", view: self.view)
-        self.storyButton = self.viewModel.buttonInMenu(Constant.Positioning.XPositionRightMenuButton, yPosition: Constant.Size.DeviceHeight / 7.5, image: "projects-image", text: "WORK", view: self.view)
-        self.storyButton = self.viewModel.buttonInMenu(Constant.Positioning.XPositionLeftMenuButton, yPosition: Constant.Size.DeviceHeight / 7.5 + Constant.Positioning.HeightMenuButton + 45, image: "skills-image", text: "SKILLSET", view: self.view)
-        self.storyButton = self.viewModel.buttonInMenu(Constant.Positioning.XPositionRightMenuButton, yPosition: Constant.Size.DeviceHeight / 7.5 + Constant.Positioning.HeightMenuButton + 45, image: "video-image", text: "VIDEO", view: self.view)
-        self.storyButton = self.viewModel.buttonInMenu(Constant.Positioning.XPositionLeftMenuButton, yPosition: Constant.Size.DeviceHeight / 7.5 + (Constant.Positioning.HeightMenuButton * 2) + 90, image: "game-image", text: "GAME", view: self.view)
-        self.storyButton = self.viewModel.buttonInMenu(Constant.Positioning.XPositionRightMenuButton, yPosition: Constant.Size.DeviceHeight / 7.5 + (Constant.Positioning.HeightMenuButton * 2) + 90, image: "contact-image", text: "CONTACT", view: self.view)
+        instantiateAllButtons()
+    }
 
+    // MARK: Helper methods
+
+    func instantiateAllButtons() {
+        self.storyButton = self.viewModel.buttonInMenu(
+            Constant.Positioning.XPositionLeftMenuButton,
+            yPosition: Constant.Size.DeviceHeight / 7.5,
+            image: "story-image",
+            text: "STORY",
+            view: self.view)
+
+        self.projectsButton = self.viewModel.buttonInMenu(
+            Constant.Positioning.XPositionRightMenuButton,
+            yPosition: Constant.Size.DeviceHeight / 7.5,
+            image: "projects-image",
+            text: "WORK",
+            view: self.view)
+
+        self.skillsetButton = self.viewModel.buttonInMenu(
+            Constant.Positioning.XPositionLeftMenuButton,
+            yPosition: Constant.Size.DeviceHeight / 7.5 + Constant.Positioning.HeightMenuButton + 45,
+            image: "skills-image",
+            text: "SKILLSET",
+            view: self.view)
+
+        self.videoButton = self.viewModel.buttonInMenu(
+            Constant.Positioning.XPositionRightMenuButton,
+            yPosition: Constant.Size.DeviceHeight / 7.5 + Constant.Positioning.HeightMenuButton + 45,
+            image: "video-image",
+            text: "VIDEO",
+            view: self.view)
+
+        self.gameButton = self.viewModel.buttonInMenu(
+            Constant.Positioning.XPositionLeftMenuButton,
+            yPosition: Constant.Size.DeviceHeight / 7.5 + (Constant.Positioning.HeightMenuButton * 2) + 90,
+            image: "game-image",
+            text: "GAME",
+            view: self.view)
+
+        self.contactButton = self.viewModel.buttonInMenu(
+            Constant.Positioning.XPositionRightMenuButton,
+            yPosition: Constant.Size.DeviceHeight / 7.5 + (Constant.Positioning.HeightMenuButton * 2) + 90,
+            image: "contact-image",
+            text: "CONTACT",
+            view: self.view)
     }
 }

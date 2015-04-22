ba17603b96071aeaf7b159d66df838211016a645
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-22 09:57:28 +0200

    Added some new code

--- RamonGilabert/RamonGilabert/RGProjectsViewController.swift
+++ RamonGilabert/RamonGilabert/RGProjectsViewController.swift
@@ -5,10 +5,14 @@ class RGProjectsViewController: UIViewController {
     let viewModel = ViewModel()
     var scrollView = UIScrollView()
     var mainView = UIView()
+    var backgroundImageView = UIImageView()
+
+    // MARK: View lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.backgroundImageView = self.viewModel.setBackgroundProjects(self.view)
         self.mainView = self.viewModel.setMainView(self.view)
     }
 }

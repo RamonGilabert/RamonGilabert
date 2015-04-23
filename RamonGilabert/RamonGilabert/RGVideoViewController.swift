7bd909249af8148148489bac6e727c6c74e0cd7a
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-23 23:02:17 +0200

    Small refactor

--- RamonGilabert/RamonGilabert/RGVideoViewController.swift
+++ RamonGilabert/RamonGilabert/RGVideoViewController.swift
@@ -18,22 +18,14 @@ class RGVideoViewController: UIViewController {
         self.session.setCategory(AVAudioSessionCategoryPlayback, error: nil)
 
         self.moviePlayerController = MPMoviePlayerController(contentURL: Video.MainVideoURL)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "moviePlayerDidFinishPlaying:" , name: MPMoviePlayerPlaybackDidFinishNotification, object: self.moviePlayerController)
-        self.moviePlayerController.scalingMode = MPMovieScalingMode.AspectFill
-        self.moviePlayerController.controlStyle = MPMovieControlStyle.None
-        self.moviePlayerController.backgroundView.backgroundColor = UIColor(red:0.11, green:0.1, blue:0.11, alpha:1)
-        self.moviePlayerController.repeatMode = MPMovieRepeatMode.None
         self.moviePlayerController.view.frame = CGRectMake(0, 0, Constant.Size.DeviceWidth, Constant.Size.DeviceHeight)
-        self.moviePlayerController.allowsAirPlay = true
-        self.moviePlayerController.prepareToPlay()
         self.view.addSubview(self.moviePlayerController.view)
-        self.view.bringSubviewToFront(self.moviePlayerController.view)
-        self.moviePlayerController.play()
 
-        let crossButton = UIButton(frame: CGRectMake(20, 20, 28, 28))
-        crossButton.addTarget(self, action: "onCrossButtonPressed", forControlEvents: UIControlEvents.TouchUpInside)
-        crossButton.setBackgroundImage(UIImage(named: "cross-button-shadow"), forState: UIControlState.Normal)
-        self.view.addSubview(crossButton)
+        configureMoviePlayer()
+        addObserverToMoviePlayer()
+        crossButtonLayout()
+
+        self.moviePlayerController.play()
     }
 
     // MARK: Notification methods
@@ -49,5 +41,27 @@ class RGVideoViewController: UIViewController {
         self.dismissViewControllerAnimated(true, completion: { () -> Void in
             self.moviePlayerController.stop()
         })
+    }
+
+    // MARK: MoviePlayer configuration
+
+    func crossButtonLayout() {
+        let crossButton = UIButton(frame: CGRectMake(20, 20, 28, 28))
+        crossButton.addTarget(self, action: "onCrossButtonPressed", forControlEvents: UIControlEvents.TouchUpInside)
+        crossButton.setBackgroundImage(UIImage(named: "cross-button-shadow"), forState: UIControlState.Normal)
+        self.view.addSubview(crossButton)
+    }
+
+    func configureMoviePlayer() {
+        self.moviePlayerController.scalingMode = MPMovieScalingMode.AspectFill
+        self.moviePlayerController.controlStyle = MPMovieControlStyle.None
+        self.moviePlayerController.backgroundView.backgroundColor = UIColor_WWDC.backgroundDarkGrayColor()
+        self.moviePlayerController.repeatMode = MPMovieRepeatMode.None
+        self.moviePlayerController.allowsAirPlay = true
+        self.moviePlayerController.prepareToPlay()
+    }
+
+    func addObserverToMoviePlayer() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "moviePlayerDidFinishPlaying:" , name: MPMoviePlayerPlaybackDidFinishNotification, object: self.moviePlayerController)
     }
 }
--- RamonGilabert/RamonGilabert/UIColor+WWDC.swift
+++ RamonGilabert/RamonGilabert/UIColor+WWDC.swift
@@ -26,6 +26,10 @@ class UIColor_WWDC: NSObject {
         return UIColor(red:0.36, green:0.37, blue:0.45, alpha:1)
     }
 
+    class internal func backgroundDarkGrayColor() -> UIColor {
+        return UIColor(red:0.11, green:0.1, blue:0.11, alpha:1)
+    }
+
     class internal func tipsColor() -> UIColor {
         return UIColor(red:0.97, green:0.97, blue:0.97, alpha:1)
     }

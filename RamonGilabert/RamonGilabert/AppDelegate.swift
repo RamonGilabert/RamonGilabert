3c7252a183ddc6f20448a861305ddb9859266f6c
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-24 10:14:04 +0200

    Deleted attributions

--- RamonGilabert/RamonGilabert/AppDelegate.swift
+++ RamonGilabert/RamonGilabert/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  RamonGilabert
-//
-//  Created by Ramon Gilabert Llop on 4/18/15.
-//  Copyright (c) 2015 Ramon Gilabert. All rights reserved.
-//
-
 import UIKit
 
 struct Video {
